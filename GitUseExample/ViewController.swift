bffaaa95ec75790916c780538ff4a52f738d109f
Jrming <jmlai.me92g@g2.nctu.edu.tw>
2019-12-12 14:17:17 +0800

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Jr-Ming Lai on 2019/12/12.
+//  Copyright © 2019 Jr-Ming Lai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
